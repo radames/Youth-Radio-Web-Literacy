8ed1b35ad1c62ced93fd4b7a34cea34dec648eb6
Radames Ajna <radamajna@gmail.com>
2017-11-08 17:55:27 -0800

    circles and bar graph

--- p5.js-data-vis/script.js
+++ p5.js-data-vis/script.js
@@ -20,12 +20,48 @@ function setup() {
 
 function draw() {
   push();
+  noStroke();
   scale(scaleValue);
   background('#D6F5D6');
-  fill(100,0,0);
-  ellipse(50,50, 100,100);
-  fill(255,0,0);
-  ellipse(150,150, 200,200);
+  //fill(red, gree, blue)
+  var daca_2017 = 201678;
+  var daca_2018 = 275344;
+  var daca_2019 = 321920;
+  var scaleNumber = 1/4000;
+  
+  textAlign(CENTER);
+  fill('#AACC11');
+  ellipse(50,100, daca_2017*scaleNumber ,daca_2017*scaleNumber);
+  fill('#000000');
+  text("DACA 2017", 50,150)
+  
+  fill('#AACC11');
+  ellipse(150,100, daca_2018*scaleNumber ,daca_2018*scaleNumber);
+  fill('#000000');
+  text("DACA 2018", 150,150)
+  
+  fill('#AACC11');
+  ellipse(250,100, daca_2019*scaleNumber ,daca_2019*scaleNumber);
+  fill('#000000');
+  text("DACA 2019", 250,150)
+  
+  
+  var barW = 80;
+  fill('#AACC11');
+  rect(50-barW/2, 300, barW, -daca_2017*scaleNumber );
+  fill('#000000');
+  text("DACA 2017", 50, 330)
+
+  fill('#AACC11');
+  rect(150-barW/2, 300, barW, -daca_2018*scaleNumber );
+  fill('#000000');
+  text("DACA 2018", 150, 330)
+
+  fill('#AACC11');
+  rect(250-barW/2, 300, barW,  -daca_2019*scaleNumber );
+  fill('#000000');
+  text("DACA 2019", 250, 330)
+  
   pop();
 }
 
